e36207d3a36de82242a871e158c53935d5f27265
andjelijastankovic <andjelijastankovic95@gmail.com>
2022-10-18 20:02:31 +0200

    petlje

--- 09_18_10/index.js
+++ 09_18_10/index.js
@@ -33,3 +33,50 @@ console.log(a[3][0]);
 
 var c = 'peti element';
 console.log(c[3]);
+
+console.log('Loops');
+
+//while loop
+var i = 0;
+while(i < 10) {
+    console.log(i);
+    i++;
+}
+
+//do-while petlja (proci ce prvi put iako uslov u while nije zadovoljen)
+do{
+    console.log(i);
+    i++;
+} while(i < 10);
+
+//for petlja
+for(a = 0; a < 5; a++) {
+    console.log(a);
+
+    for(i = 4; i > a; i--) {
+        console.log('value of i is ' +i, ', and value of a is ' +a);
+    }
+}
+
+
+//for-in petlja
+var negativeNumbers = [2, 3, -5, -7, 19, -9, 0];
+var n = 0;
+var lenght = negativeNumbers.length;
+
+for(var i = 0; i < lenght; i++) {
+    var element = negativeNumbers[i];
+    if(element < 0) {
+        console.log('Negative number: ', element);
+    }
+}
+
+console.log('foreach ili for-in petlja:');
+
+//  for(var element in negativeNumbers) {
+//  if(element < 0) {
+        //console.log('For-in petlja sa negativnim brojevima', element);
+//  }
+//}
+
+
