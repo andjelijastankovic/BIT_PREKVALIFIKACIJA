fa8ddc62c7ff3a0722048601223ecd7d9421cdac
andjelijastankovic <andjelijastankovic95@gmail.com>
2022-12-23 18:52:01 +0100

    bbo - done

--- 32_browser_built_in_objects/exercize/exercize.js
+++ 32_browser_built_in_objects/exercize/exercize.js
@@ -136,3 +136,124 @@ function removeFromStorage() {
     sessionStorage.clear();
 }
 //removeFromStorage();
+
+
+/*
+    Window Methods
+    Create a function that shows the user a greeting message using alert
+    Then a question is presented to the user using prompt
+    When the user provides the answer, that answer is used in the confirm dialog
+    The format of the message in the dialog should be: "We will submit this answer now! " + the answer
+    If the user clicks OK, show alert with success message
+    If the user clicks Cancel, don't show anything
+*/
+
+function greeting() {
+    alert('The Earth says "Hello"!');
+    var question = prompt('What do you think about this page?');
+    if(question != null) {
+        var yesNo = confirm('We will submit this answer now!' + ' ' + question);
+        if(yesNo) {
+            alert('Successfully submitted!');
+        }
+    }
+}
+//greeting();
+
+/*
+    Random
+    Create a function that builds an array of 10 random numbers between 1 and 50.
+    The function prints that array out in the console and then returns it.
+*/
+console.log('random numbers function');
+function randomNumbersArray() {
+    var arr = [];
+
+    for(var i = 0; i < 10; i++) {
+        arr.push(Math.floor(Math.random() * 50) + 1);
+    }
+    console.log(arr);
+    return arr;
+}
+randomNumbersArray();
+
+/*
+    Round
+    Create a function that uses the passed array of numbers and rounds all its elements to two decimals.
+    Print out the modified array in the console.
+    Use the first function for generating the input array.
+*/
+console.log('round function');
+function roundTwoDecimals() {
+    var arr = randomNumbersArray();
+    var nArr = [];
+    for(var i = 0; i < arr.length; i++) {
+        nArr.push(arr[i].toFixed(2));
+    }
+    console.log(nArr);
+    return nArr;
+}
+roundTwoDecimals();
+
+/*
+    Floor
+    Create a function that uses the passed array of numbers 
+    and rounds all its elements to the nearest integer.
+    Print out the modified array in the console.
+    Use the first function for generating the input array.
+*/
+console.log('floor function');
+function floor() {
+    var arr = randomNumbersArray();
+    var nArr = [];
+    for(var i = 0; i < arr.length; i++) {
+        nArr.push(Math.floor(arr[i]));
+    }
+    console.log(nArr);
+    return nArr;
+}
+floor();
+
+/*        
+    Max
+    Create a function that finds and prints out 
+    the biggest element in the passed array of numbers.
+*/
+console.log('max number function');
+function maxNum() {
+    var arr = randomNumbersArray();
+    console.log(Math.max(...arr));
+}
+maxNum();
+
+/*
+    Date
+        
+    Print out the whole date object in the console.
+    Print out the current time in the console.
+    Print out the current date in the console.
+*/
+
+function wholeDate() {
+    var date = new Date();
+    console.log(date);
+}
+wholeDate();
+
+function printTime() {
+    var date = new Date();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+    console.log(`${hours}:${minutes}:${seconds}`);
+}
+printTime();
+
+function printDate() {
+    var date = new Date();
+    var day = date.getUTCDay();
+    var month = date.getUTCMonth() + 1;
+    var year = date.getUTCFullYear();
+    console.log(`${day}.${month}.${year}.`);
+}
+printDate();
