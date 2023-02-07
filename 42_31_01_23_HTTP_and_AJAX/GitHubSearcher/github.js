a6829a7919d0d3673f37c6f914b2ccd8a91e4f3e
andjelijastankovic <andjelijastankovic95@gmail.com>
2023-02-07 22:33:53 +0100

    part I of github searcher works fine

--- 42_31_01_23_HTTP_and_AJAX/GitHubSearcher/github.js
+++ 42_31_01_23_HTTP_and_AJAX/GitHubSearcher/github.js
@@ -5,75 +5,29 @@ $(document).ready(function() {
         event.preventDefault();
         
         var searchValue = $('#search').val();
-        
-
         gitSearch(searchValue);
 
     });
+
+    $('#search').change(function(){
+        var search = $('#search').val();
+
+        gitSearch(search);
+    })
 });
 
 
 
 function gitSearch(username) {
-    
+    $('.showUsers').empty();
+
     $.get(`https://api.github.com/search/users?q=${username}in:user`,
     function(data) {
         console.log(data);
-
         data.items.forEach(item => {
-            user = `<img src='${item.avatar_url}'><br><p>${item.login}</p>`;
+            user = `<div class='grid-item'><a target='_blank' href='${item.html_url}'><img src='${item.avatar_url}'></a><p>${item.login}</p></div>`;
             $('.showUsers').append(user);
         });
     })
-
-    /*
-    $('form').submit(function(event){
-        
-            var searchValue = $('#search').val();
-            var request = $.ajax({
-                url: 'https://api.github.com/search/users?q='+searchValue,
-                method: 'GET'
-            });
-
-            request.done(function(response){
-                $.each(response,function(index, element) {
-                    console.log(element);
-                })
-            });
-
-            request.fail(function(response){
-                console.log('Ooops, something went wrong');
-            });
-        
-    })
-    */
-    /*
-    $('#search').on('keyup', function(event){
-        if(event.key == 'Enter') {
-            event.preventDefault();
-            var searchValue = $('#search').val();
-            var request = $.ajax({
-                url: 'https://api.github.com/search/users?q='+searchValue,
-                method: 'GET'
-            });
-
-            request.done(function(response){
-                $.each(response,function(index, element) {
-                    console.log(element);
-                })
-            });
-
-            request.fail(function(response){
-                console.log('Ooops, something went wrong');
-            });
-            
-        }
-    });
-    */
-    
-
-
-    
 }
-
 
