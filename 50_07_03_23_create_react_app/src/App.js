5a1c5f5ffc8b8ce2757785e58b97fc6bb2e5da1c
andjelijastankovic <andjelijastankovic95@gmail.com>
2023-03-07 19:19:05 +0100

    create react app

---
+++ 50_07_03_23_create_react_app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { HelloWorld } from './components/HelloWorld';
+import HelloWorldFunc from './components/HelloWorldFunc';
+
+function App() {
+  return (
+    <div>
+      <HelloWorld/>
+      <HelloWorldFunc/>
+    </div>
+  );
+}
+
+export default App;
---
+++ 50_07_03_23_create_react_app/src/components/HelloWorld.jsx
@@ -0,0 +1,8 @@
+import { Component } from "react";
+
+export class HelloWorld extends Component {
+    render() {
+        const text = 'Hello world';
+        return <h1>{text}</h1>;
+    }
+}
