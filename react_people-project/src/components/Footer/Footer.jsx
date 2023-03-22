d96503faceb02e2ca6c50148af37031e31eb131f
andjelijastankovic <andjelijastankovic95@gmail.com>
2023-03-23 00:45:23 +0100

    change time

--- react_people-project/src/components/Footer/Footer.jsx
+++ react_people-project/src/components/Footer/Footer.jsx
@@ -10,26 +10,34 @@ export const Footer = () => {
         } else if(updateTimeSeconds > 60 && updateTimeSeconds < 3600) {
             let minute = Math.floor(updateTimeSeconds / 60);
             return `${minute} minutes ago`;
-        } else if(updateTimeSeconds === 3600) {
-            return `1 hour ago`;
         } else if(updateTimeSeconds > 3600 && updateTimeSeconds < 86400) {
             let hours = Math.floor(updateTimeSeconds / 60 / 60);
-            return `${hours} hours ago`;
-        } else if(updateTimeSeconds === 86400) {
-            return `1 day ago`;
+            if(hours === 1) {
+                return '1 hour ago';
+            } else {
+                return `${hours} hours ago`;
+            }
         } else if(updateTimeSeconds > 86400 && updateTimeSeconds < 604799) {
             let days = Math.floor(updateTimeSeconds / 60 / 60 / 24);
-            return `${days} days ago`;
-        } else if(updateTimeSeconds === 604800) {
-            return `1 week ago`;
+            if(days === 1) {
+                return `1 day ago`;
+            } else {
+                return `${days} days ago`;
+            }
         } else if(updateTimeSeconds > 604800 && updateTimeSeconds < 2629799) {
             let weeks = Math.floor(updateTimeSeconds / 60 / 60 / 24 / 7);
-            return `${weeks} weeks ago`;
-        } else if(updateTimeSeconds === 2629800) {
-            return `1 month ago`;
+            if(weeks === 1) {
+                return `1 week ago`;
+            } else {
+                return `${weeks} weeks ago`;
+            }
         } else if(updateTimeSeconds > 2629800 && updateTimeSeconds < 31557599) {
             let months = Math.floor(updateTimeSeconds / 2629800);
-            return `${months} months ago`;
+            if(months === 1) {
+                return `1 month ago`;
+            } else {
+                return `${months} months ago`;
+            }
         } else if(updateTimeSeconds > 31557600) {
             return `over a year ago`;
         } else {
